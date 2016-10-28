9ed22180df3409ae7ee80c366c95712eebcc12c7
a medical student <x2yline@users.noreply.github.com>
2016-10-29 00:43:52 +0800

    Update ncbi.py

--- ncbi.py
+++ ncbi.py
@@ -49,18 +49,18 @@ def genbank_url(fasta_urls):
 
 def get_fasta_sq(fasta_urls):
     #fasta_urls is the urls modified by function fasta_url
-    global object,GID
+    global object,GID,gene_name
 
-    new = os.path.join(os.getcwd(),GID+" "+object)
+    new = os.path.join(os.getcwd(),gene_name +" "+object)
     try:
         os.makedirs(new)
-        print 'have create the new directory ' + str(new)
+        # print 'have create the new directory ' + str(new)
     except:
-         print "can't create a new directory"
+        print "can't create a new directory"
     if type(fasta_urls) == type([]):
         for i in fasta_urls:
             t = etree.HTML(urllib.urlopen(i).read())
-            print 'have got the fasta page'
+            # print 'have got the fasta page'
         # //*[@id="gi_1036030432"]/div/div[2]/p[3]/span[1]
         # //*[@id="gi_1036030432"]/div/div[2]/p[1]/a
             title1 = t.xpath('//*[@id="content"]/div/div[1]/div/div[2]/p[3]/span[1]/text()')[0].split('G')[0]
@@ -73,16 +73,10 @@ def get_fasta_sq(fasta_urls):
             f = open(title,'w')
             f.write(t)
             f.close()
-            print 'Have dowloaded the ' + object + ': ' + title
+            # print 'Have dowloaded the ' + object + ': ' + title
     else:
-        try:
-            os.makedirs(new)
-            print 'have create the new directory ' + str(new)
-        except:
-            print "can't create a new directory"
-
         t = etree.HTML(urllib.urlopen(fasta_urls).read())
-        print 'have got the fasta page'
+        # print 'have got the fasta page'
         # //*[@id="gi_1036030432"]/div/div[2]/p[3]/span[1]
         # //*[@id="gi_1036030432"]/div/div[2]/p[1]/a
         title1 = t.xpath('//*[@class="aux"]/span[1]/text()')[0].split('G')[0]
@@ -95,7 +89,7 @@ def get_fasta_sq(fasta_urls):
         f = open(title,'w')
         f.write(t)
         f.close()
-        print 'Have dowloaded the ' + object + ': ' + title 
+        # print 'Have dowloaded the ' + object + ': ' + title 
 
 
 def get_non_DNA_seq(url):
@@ -111,17 +105,17 @@ def get_non_DNA_seq(url):
 
 def get_DNA_seq(fasta_urls):
     #fasta_urls is DNA_fasta_url like: https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=1036030432&db=nuccore&report=fasta&from=0&to=20
-    global object
+    global object, gene_name
     global GID
-    new = os.path.join(os.getcwd(), GID +" "+object)
+    new = os.path.join(os.getcwd(), gene_name +" "+object)
     try:
         os.makedirs(new)
-        print 'have create the new directory ' + str(new)
+        # print 'have create the new directory ' + str(new)
     except:
          print "can't create a new directory"
 
     t = etree.HTML(urllib.urlopen(fasta_urls).read())
-    print 'have got the fasta page'
+    # print 'have got the fasta page'
         # //*[@id="gi_1036030432"]/div/div[2]/p[3]/span[1]
         # //*[@id="gi_1036030432"]/div/div[2]/p[1]/a
     title1 = t.xpath('//*[@class="aux"]/span[1]/text()')[0].split('G')[0]
@@ -134,7 +128,7 @@ def get_DNA_seq(fasta_urls):
     f = open(title,'w')
     f.write(t)
     f.close()
-    print 'Have dowloaded the ' + 'DNA' + ': ' + title 
+    # print 'Have dowloaded the ' + 'DNA' + ': ' + title 
 
 
 
@@ -144,16 +138,17 @@ def get_DNA_seq(fasta_urls):
 
 
 def main(number):
-    global object, GID
+    global object, GID, gene_name
     try:
-        print 'Checking your internet...\n'
-        urllib.urlopen('https://www.baidu.com/')
-        print 'your Internet Works Well\n'
+        # print 'Checking your internet...\n'
+        # urllib.urlopen('https://www.baidu.com/')
+        # print 'your Internet Works Well\n'
         
         if number[0] == '0' or number[0] == '1' or number[0] == '2' or number[0] == '3' or number[0] == '4' or number[0] == '5' or number[0] == '6' or number[0] == '7' or number[0] == '8' or number[0] == '9':
             gene_url='https://www.ncbi.nlm.nih.gov'+'/gene/'+number
-            print 'loading......'
+            print '\nloading......\n'
             html = etree.HTML(urllib.urlopen(gene_url).read())
+            gene_name = html.xpath('//*[@id="summaryDl"]/dd[1]/text()')[0]
             protein_url = 'https://www.ncbi.nlm.nih.gov/protein?LinkName=gene_protein_refseq&from_uid=' + number
             RNA_url = 'https://www.ncbi.nlm.nih.gov/nuccore?LinkName=gene_nuccore_refseqrna&from_uid='+ number
             DNA_url = 'https://www.ncbi.nlm.nih.gov'+html.xpath('//*[@class="divAccession"]/div/p/a[2]/@href')[0]
@@ -167,28 +162,46 @@ def main(number):
             get_non_DNA_seq(protein_url)
             print 'have done all things with Protein\n'
             object = 'DNA'
-            print 'Begin with DNA...' +  DNA_fasta_url
+            # print 'Begin with DNA...' +  DNA_fasta_url
             get_DNA_seq(DNA_fasta_url)
             print 'have done all things with DNA\n'
-            raw_input('Done all things plese press ENTER to quit :)')
+
         else:
             object = number
             url = "https://www.ncbi.nlm.nih.gov/nuccore/" + number
-            print url
+            print '\nloading......\n'
             html = etree.HTML(urllib.urlopen(url).read())
-            print 'page got'
+            # gene_name = html.xpath('//*[@id="EntrezForm"]/div[1]/div[6]/div/div[8]/div[1]/div/h3/text()')[0]
+            gene_name = ' '
+            # print 'page got'
             GID = html.xpath('//*[@class="dblinks"]/@href')[0].split('/')[-1].split('?')[0]
-            print GID
+            print 'the current dowloading GI is ' + GID
             fasta_urls = fasta_url(GID)[0]
-            print fasta_urls
+            # print fasta_urls
             get_fasta_sq(fasta_urls)
-    except:
-        print 'There is something wrong with your intenet or other bugs'
+            
+    except Exception as e:
+        print e
 
-f=open('input.txt','r')
-number = f.readlines
+input = os.path.join(os.getcwd(),'input.txt')
+f=open(input,'r')
+numbers = f.readlines()
 f.close()
+try:
+    print 'Checking your internet...'
+    urllib.urlopen('https://www.baidu.com/')
+    print 'your Internet Works Well\n'
+    for i in numbers:
+        i.strip()
+        i = i.split()
+        for j in i:
+            j = j.split('\n')
+            for p in j:
+                if p:
+                    main(p)
 
-for i in number:
-    i.strip()
-    main(i)
+    raw_input('Done all things plese press ENTER to quit :)')
+
+except Exception as e:
+    print e
+    raw_input("It don't work plese press ENTER to quit :(")
